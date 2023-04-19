544b798b7f517211f8b2e54aca879bcf4844a924
Bruno Flores <117491562+brunoflvieira@users.noreply.github.com>
2023-04-18 22:38:08 -0300

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,32 @@
+package Principal;
+
+import java.net.*;
+import java.io.*;
+
+public class Principal {
+
+    public static void main(String args[]) {
+        try {
+            DatagramSocket con = new DatagramSocket(62720); //conexao(porta)
+            while (true) {
+                byte dado[] = new byte[100]; //Configura o pacote
+                DatagramPacket pacote = new DatagramPacket(dado, dado.length); //Configura o pacote
+                System.out.println("Inicio servidor");
+                String mensagem = "";
+                while (!mensagem.equals("fim")) {
+                    con.receive(pacote); //espera pacote
+                    System.out.println("\n\nPacote recebido do Cliente :"
+                            + "\n do Host: " + pacote.getAddress()
+                            + "\n da Porta: " + pacote.getPort()
+                            + "\n de Tamanho: " + pacote.getLength()
+                            + "\n contendo: ");
+//Recupera o texto recebido
+                    mensagem = new String(pacote.getData(), 0, pacote.getLength());
+                    System.out.println(" " + mensagem); //escreve mensagem
+                }
+            }
+        } catch (IOException io) {
+            System.err.println("Problemas de IO");
+        }
+    }
+}
